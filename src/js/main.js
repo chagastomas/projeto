bfdde686b2864ec9d9bbd2f8a467c3060e1fa52f
tomas chagas <tomas.chagas@infracommerce.com.br>
2020-11-15 03:25:06 -0300

    create carroussel products

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,66 @@
 $(document).ready(function () {
   $.get('https://sky-frontend.herokuapp.com/movies', function (data) {
     let dataMovies = data[2].movies;
+    let dataHighlights = data[0].items;
     for (let i = 0; i < data[2].movies.length; i++) {
-      switch (dataMovies[i].categories) {
-        case 'Ação e Aventura, Infantil':
-          $('.wrapper-movies-action .teste').append(
-            '<img src=' + dataMovies[i].images[0].url + ' />'
-          );
-          break;
-        case 'Animação, Infantil':
-          $('.wrapper-movies-animation').append('<img src=' + dataMovies[i].images[0].url + ' />');
-          break;
-        case 'Suspense, Terror':
-          $('.wrapper-movies-horror').append('<img src=' + dataMovies[i].images[0].url + ' />');
-          break;
-      }
+      $('.wrapper-movies .photos').append(
+        '<a href="#"><img src=' + dataMovies[i].images[0].url + ' /></a>'
+      );
     }
-    // $('.wrapper-movies-action').slick();
-    // $('.wrapper-movies-action .teste').slick({
-    //   dots: true,
-    //   infinite: false,
-    //   speed: 300,
-    //   slidesToShow: 1,
-    //   slidesToScroll: 1,
-    //   responsive: [
-    //     {
-    //       breakpoint: 768,
-    //       settings: {
-    //         slidesToShow: 2,
-    //         slidesToScroll: 2,
-    //       },
-    //     },
-    //     {
-    //       breakpoint: 490,
-    //       settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //       },
-    //     },
-    //   ],
-    // });
+    for (let i = 0; i < data[0].items.length; i++) {
+      $('.wrapper-movies-highlights .photos').append(
+        '<a href="#"><img src=' + dataHighlights[i].images[0].url + ' /></a>'
+      );
+    }
+    carroussel();
   });
 });
+
+function carroussel() {
+  $('.wrapper-movies .photos').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 490,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $('.wrapper-movies-highlights .photos').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 490,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+}
